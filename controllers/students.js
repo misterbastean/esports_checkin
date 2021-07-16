f834557b507ddf1b6b055e8ca24d37feff8cf8d5
Josh Bastean <misterbastean@gmail.com>
2021-07-16 14:59:36 -0400

    Limit fields for getStudents; fix error handling in getStudent

--- controllers/students.js
+++ controllers/students.js
@@ -2,8 +2,8 @@ const Student = require('../models/student')
 const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse');
 
-exports.getStudents = asyncHandler(async (req, res) => {
-  const data = await Student.find();
+exports.getStudents = asyncHandler(async (req, res, next) => {
+  const data = await Student.find().select('studentId firstName lastName email flag preferredEsport').exec();
   res.status(200).json({
     success: true,
     count: data.length,
@@ -11,7 +11,20 @@ exports.getStudents = asyncHandler(async (req, res) => {
   })
 })
 
-exports.createStudent = asyncHandler(async (req, res) => {
+exports.getStudent = asyncHandler(async (req, res, next) => {
+  const data = await Student.find({studentId: req.params.id})
+  if (!data || data.length === 0) {
+    return next(
+      new ErrorResponse(`Student not found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data
+  })
+})
+
+exports.createStudent = asyncHandler(async (req, res, next) => {
   const newStudent = {
     studentId: req.body.studentId,
 		firstName: req.body.firstName,
@@ -24,5 +37,4 @@ exports.createStudent = asyncHandler(async (req, res) => {
   }
 
   const data = await Student.create(newStudent)
-
 })
