9de60863ff6dfa39a38068c04a54388512c719be
Josh Bastean <misterbastean@gmail.com>
2021-07-29 09:53:41 -0400

    Refactor logic; Add logic for handling missed punches; Prettier style

--- controllers/students_client.js
+++ controllers/students_client.js
@@ -1,70 +1,77 @@
-const moment = require('moment')
-const axios = require('axios')
-const Student = require('../models/student')
-const asyncHandler = require('../middleware/async')
-
+const moment = require('moment');
+const axios = require('axios');
+const Student = require('../models/student');
+const asyncHandler = require('../middleware/async');
+const getMinutesBetweenDates = require('../utils/getMinutesBetweenDates');
 
 exports.index = (req, res) => {
-  res.render("index")
-}
+  res.render('index');
+};
 
 exports.students = asyncHandler(async (req, res, next) => {
-  const students = await Student.find({}).select("studentId firstName lastName preferredEsport flag").exec();
-  res.render("students", { students })
-})
+  const students = await Student.find({})
+    .select('studentId firstName lastName preferredEsport flag')
+    .exec();
+  res.render('students', { students });
+});
 
 exports.studentsNew = (req, res) => {
-  res.render("studentsNew")
-}
+  res.render('studentsNew');
+};
 
 exports.studentsCreate = (req, res) => {
-  axios.post("http://localhost:3000/api/v1/students", req.body)
-  .then((response) => {
-    console.log("response:", response)
-    res.redirect("/")
-  })
-  .catch((err) => {
-    console.log("err:", err);
-    res.redirect("/")
-  })
-
-}
+  axios
+    .post('http://localhost:3000/api/v1/students', req.body)
+    .then((response) => {
+      console.log('response:', response);
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log('err:', err);
+      res.redirect('/');
+    });
+};
 
 exports.studentsShow = asyncHandler(async (req, res, next) => {
-  const student = await Student.findOne({studentId: req.params.id});
+  const student = await Student.findOne({ studentId: req.params.id });
 
   // Punch formatting for time
-  const punches = []
+  const punches = [];
   student.punches.forEach((punch) => {
-    const date = moment(punch.rawDate).format("M/DD/YY")
-    const time = moment(punch.rawDate).format("hh:mm A")
+    const date = moment(punch.rawDate).format('M/DD/YY');
+    const time = moment(punch.rawDate).format('hh:mm A');
 
     punches.push({
       date,
       time,
-      type: punch.type
-    })
+      type: punch.type,
+    });
   });
 
   // Total time
   let totalTime = 0;
   for (let i = 0; i < student.punches.length; i += 2) {
     // Check types
-    if (student.punches[i].type === "in" && student.punches[i+1] !== undefined && student.punches[i+1].type === "out") {
-      const punchIn = student.punches[i].rawDate
-      const punchOut = student.punches[i+1].rawDate
+    if (
+      (student.punches[i].type === 'in' || student.punches[i].type === 'in (missed)') &&
+      student.punches[i + 1] !== undefined &&
+      (student.punches[i + 1].type === 'out' || student.punches[i + 1].type === 'out (missed)')
+    ) {
+      const punchIn = student.punches[i].rawDate;
+      const punchOut = student.punches[i + 1].rawDate;
+      const diffMinutes = getMinutesBetweenDates(punchIn, punchOut);
+      totalTime += diffMinutes;
+    } else if (student.punches[i].type === 'in' && student.punches[i + 1] === undefined) {
+      // Did not punch out
+      const punchIn = student.punches[i].rawDate;
+      const punchOut = new Date();
       const diffMinutes = Math.round((((punchOut - punchIn) % 86400000) % 3600000) / 60000);
-      totalTime += diffMinutes
-    } else if (student.punches[i].type === "in" && student.punches[i+1] === undefined) {  // Did not punch out
-      const punchIn = student.punches[i].rawDate
-      const punchOut = new Date()
-      const diffMinutes = Math.round((((punchOut - punchIn) % 86400000) % 3600000) / 60000);
-      totalTime += diffMinutes
+      totalTime += diffMinutes;
     } else {
       // TODO: Add error handling
-      totalTime = "ERR"
+      totalTime = 'ERR';
     }
   }
 
-  res.render("studentsShow", { student, punches, totalTime })
-})
+  res.render('studentsShow', { student, punches, totalTime });
+});
---
+++ utils/getMinutesBetweenDates.js
@@ -0,0 +1,7 @@
+function getMinutesBetweenDates(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  return Math.abs(Math.round(diff));
+}
+
+module.exports = getMinutesBetweenDates;
